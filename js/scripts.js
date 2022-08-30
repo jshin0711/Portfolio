94d2ccb479c63028775a27b389087ca2eec1881f
jshin0711 <jshin0711@gmail.com>
2022-08-29 22:09:42 -0500

    Add floating "Back to Top" button
    - Add HTML button
    - Style button using CSS
    - Add JavaScript to appear and scroll to top when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,26 @@ $(document).ready(function(){
   });
 });
 
+//Get the button:
+mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // Smooth Scrolling
 // Select all links with hashes
 $('a[href*="#"]')
