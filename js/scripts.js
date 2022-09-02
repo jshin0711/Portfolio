cf87f3d084cd831358841afe1e718c8bdfe8c766
jshin0711 <jshin0711@gmail.com>
2022-09-02 13:02:40 -0500

    Add scroll to top JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -18,22 +18,28 @@ $(document).ready(function(){
 // Return to top button
 mybutton = document.getElementById("backToTop");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {10
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // Animation replay button
 replaybutton = document.getElementById("replay")
 replaybutton.onclick = function(){location.reload();};
 
-// Smooth Scrolling
+//SmoothScroll
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
