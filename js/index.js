493ba2a28b85d189615eadb5f0a02a30698634d2
jshin0711 <jshin0711@gmail.com>
2022-10-16 19:39:27 -0500

    Code linting

--- js/index.js
+++ js/index.js
@@ -1,43 +1,42 @@
 // Top nav hamburger menu
 function hamburger () {
-  var x = document.getElementById('myNavtoggle')
+  var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += ' responsive'
+    x.className += ' responsive';
   } else {
-    x.className = 'navtoggle'
+    x.className = 'navtoggle';
   }
 }
 
 $(document).ready(function () {
   $('.hamburger').click(function () {
-    $(this).toggleClass('is-active')
-  })
-})
+    $(this).toggleClass('is-active');
+  });
+});
 
 // Return to top button
-mybutton = document.getElementById('backToTop')
+var mybutton = document.getElementById('backToTop');
 
 // When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function () {
-  scrollFunction()
-}
+  scrollFunction();
+};
 
 function scrollFunction () {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    10
-    mybutton.style.display = 'block'
+    mybutton.style.display = 'block';
   } else {
-    mybutton.style.display = 'none'
+    mybutton.style.display = 'none';
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction () {
-  document.body.scrollTop = 0 // For Safari
-  document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 // Replay animation
 function refreshPage () {
-  window.location.reload()
+  window.location.reload();
 }
